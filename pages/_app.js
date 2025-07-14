42a7fb0da6e593e50ff0c506223256f67bb0d371
Joseph Knittel <knitteljoseph@gmail.com>
2025-07-14 00:18:29 -0500

    Add Solyn personalized subject response

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 'use client'
+import '../styles/globals.css'
+import { Fredoka } from 'next/font/google'
 import { useState } from 'react'
-import './styles/global.css'
+
+const fredoka = Fredoka({ subsets: ['latin'] })
 
 export default function Home() {
   const [name, setName] = useState('')
@@ -9,15 +12,13 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
     let personalizedResponse = ''
-
     if (!name || !subject) {
       personalizedResponse = "👋 Please enter both your name and your favorite subject to get started."
     } else {
       switch(subject.toLowerCase()) {
         case 'math':
-          personalizedResponse = `🔢 Nice, ${name}! Math is like a puzzle. Let’s unlock some secrets together!`
+          personalizedResponse = `🔢 Nice, ${name}! Math is like a puzzle. Let's unlock some secrets together!`
           break
         case 'science':
           personalizedResponse = `🧪 Ooooh, science! ${name}, get ready for some experiments and discoveries.`
@@ -28,18 +29,17 @@ export default function Home() {
           personalizedResponse = `📖 Reading time, ${name}? Let me grab my favorite book!`
           break
         case 'history':
-          personalizedResponse = `📜 ${name}, let’s time travel through history together. Buckle up!`
+          personalizedResponse = `📜 ${name}, let's time travel through history together. Buckle up!`
           break
         default:
-          personalizedResponse = `🌟 Awesome choice, ${name}! I’ll help you learn all about ${subject}.`
+          personalizedResponse = `🌟 Awesome choice, ${name}! I'll help you learn all about ${subject}.`
       }
     }
-
     setResponse(personalizedResponse)
   }
 
   return (
-    <main>
+    <main className={fredoka.className}>
       <h1>Welcome to Solyn!</h1>
       <form onSubmit={handleSubmit}>
         <input
