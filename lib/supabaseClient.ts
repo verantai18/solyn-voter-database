eedbf9d55ebe64b55f5e30725631a7ff0d5b94d3
verantai18 <verantai18@gmail.com>
2025-07-19 11:01:46 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,1 +1,7 @@
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'YOUR_SUPABASE_URL'; // e.g. https://xxxx.supabase.co
+const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
