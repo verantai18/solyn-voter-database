efb5eee579a3347e31e62c7a8d98dba7f6193729
Joseph Knittel <knitteljoseph@gmail.com>
2025-07-19 15:07:07 -0500

    Fix import paths: use relative paths instead of @/ alias to resolve module resolution issues

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 export function MainNav() {
   const pathname = usePathname()
