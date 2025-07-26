b754e01e2f64644137f8012cb1d939b6c4e1d5cf
Joseph Knittel <knitteljoseph@gmail.com>
2025-07-26 10:13:38 -0500

    CLEANUP: Remove unused test and debug files

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">Test Page</h1>
-      <p>This is a test page to verify routing is working.</p>
-      <p>If you can see this, the basic routing is working!</p>
-    </div>
-  );
-} 
